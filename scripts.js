c17756619b0e371915c9c99bf7330e90e5c5e59f
ZeeCoder91 <zeenavatman@gmail.com>
2021-10-27 21:11:10 -0400

    added logic to playGame and game functions

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 function game() {
-    let playerScore;
-    let computerScore;
-    let moves;
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
 }
 
 function playGame() {
-    var playerOptions = []
+    const rockBtn = document.querySelector(".rock");
+    const paperBtn = document.querySelector(".paper");
+    const scissorBtn = document.querySelector(".scissors")
+    const playerOptions = [rockBtn, paperBtn, scissorBtn];
+    let computerOptions = ["rock", "paper", "scissors"]
+        
+    });
+}
+
+function winner() {
+
+}
+
+function winner(params) {
+    
+}
+
+function gameOver(params) {
+    
 }
