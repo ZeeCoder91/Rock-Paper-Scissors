71b894b711f7b856224c4027611a9e4188979084
ZeeCoder91 <zeenavatman@gmail.com>
2021-10-30 20:51:18 -0400

    Added function to check who wins and calling a gameover function after 10 moves

--- scripts.js
+++ scripts.js
@@ -16,10 +16,29 @@ const game = () => {
 
             const movesLeft = document.querySelector('.movesleft');
             moves++;
-            movesLeft.innerText = `Moves Left: ${10-moves}`);
-            
-        }
+            movesLeft.innerText = `Moves Left: ${10-moves}`;
+
+            const choiceNumber = Math.floor(Math.random()*3);
+            const computerChoice = computerOptions[choiceNumber];
+
+            //Function to check who wins
+            winner(this.innerText,computerChoice)
+
+            // Calling gameOver function after 10 moves
+            if(moves === 10){
+              gameOver(playerOptions, movesLeft);
+            }
+        })
       })
-    })
-}
-    
+    }
+
+
+
+
+
+
+
+
+
+
+}    
